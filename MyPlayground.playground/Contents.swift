90e1f5f7dae22e3a23ecf0c220817c9f56b6f988
soufuru <soufuru1@gmail.com>
2016-06-11 15:23:25 +0900

    first commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var array = [AnyObject]()
+
+array.append(["あ",2])
+array.append(["い",2])
+array.append(["う",2])
+
+var title = array[0][0]
+array[1]
